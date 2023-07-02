f51983af730838f2e07de4e117d4148f50b8e93f
moojing <starts789@gmail.com>
2023-07-03 01:27:17 +0800

    feat: add deploy script.

---
+++ src/script/deploy.sol
@@ -0,0 +1,13 @@
+import "forge-std/Script.sol";
+import {Orderbook} from "../Orderbook.sol";
+
+contract MyScript is Script {
+    function run() public {
+        uint deployerPrivateKey = vm.envUint("SCRIPT_PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        vm.startBroadcast(deployerPrivateKey);
+        Orderbook orderbook = new Orderbook();
+        vm.stopBroadcast();
+    }
+}
