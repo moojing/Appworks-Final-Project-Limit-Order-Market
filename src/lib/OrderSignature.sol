95fd6ca323849b1cfa57127d30691c22c25e84ce
moojing <starts789@gmail.com>
2023-07-02 20:44:13 +0800

    feat: add eip712 signature functions.

--- src/lib/OrderSignature.sol
+++ src/lib/OrderSignature.sol
@@ -1,139 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+import "forge-std/console.sol";
 import {OrderStructs} from "../lib/OrderStructs.sol";
 
-/* Signature Verification
-
-How to Sign and Verify
-# Signing
-1. Create message to sign
-2. Hash the message
-3. Sign the hash (off chain, keep your private key secret)
-
-# Verify
-1. Recreate hash from the original message
-2. Recover signer from signature and hash
-3. Compare recovered signer to claimed signer
-*/
-
 library OrderSignature {
-    /* 1. Unlock MetaMask account
-    ethereum.enable()
-    */
+    bytes32 constant EIP712DOMAIN_TYPEHASH =
+        keccak256(
+            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+        );
 
-    /* 2. Get message hash to sign
-    getMessageHash(
-        0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C,
-        123,
-        "coffee and donuts",
-        1
-    )
+    bytes32 constant MAKER_TYPEHASH =
+        keccak256(
+            "Maker(OrderType orderType,bool isFulfilled,uint256 globalNonce,uint256 subsetNonce,uint256 orderNonce,CollectionType collectionType,address collection,address currency,address signer,uint256 startTime,uint256 endTime,uint256 price,uint256[] itemIds,uint256[] amounts)"
+        );
 
-    hash = "0xcf36ac4f97dc10d91fc2cbb20d718e94a8cbfe0f82eaedc6a4aa38946fb797cd"
-    */
-    function getMessageHash(
-        OrderStructs.Maker memory order
+    bytes32 constant DOMAIN_SEPARATOR =
+        keccak256(
+            abi.encode(
+                EIP712DOMAIN_TYPEHASH,
+                keccak256("Orderbook-v1"),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
+
+    function hashStruct(
+        OrderStructs.Maker memory maker
     ) public pure returns (bytes32) {
         return
             keccak256(
-                abi.encodePacked(
-                    order.orderType,
-                    order.isFulfilled,
-                    order.globalNonce,
-                    order.subsetNonce,
-                    order.orderNonce,
-                    order.collectionType,
-                    order.collection,
-                    order.currency,
-                    order.signer,
-                    order.startTime,
-                    order.endTime,
-                    order.price,
-                    order.itemIds,
-                    order.amounts
+                abi.encode(
+                    MAKER_TYPEHASH,
+                    maker.orderType,
+                    maker.isFulfilled,
+                    maker.globalNonce,
+                    maker.subsetNonce,
+                    maker.orderNonce,
+                    maker.collectionType,
+                    maker.collection,
+                    maker.currency,
+                    maker.signer,
+                    maker.startTime,
+                    maker.endTime,
+                    maker.price,
+                    maker.itemIds,
+                    maker.amounts
                 )
             );
-    }
-
-    /* 3. Sign message hash
-    # using browser
-    account = "copy paste account of signer here"
-    ethereum.request({ method: "personal_sign", params: [account, hash]}).then(console.log)
-
-    # using web3
-    web3.personal.sign(hash, web3.eth.defaultAccount, console.log)
-
-    Signature will be different for different accounts
-    0x993dab3dd91f5c6dc28e17439be475478f5635c92a56e17e82349d3fb2f166196f466c0b4e0c146f285204f0dcb13e5ae67bc33f4b888ec32dfe0a063e8f3f781b
-    */
-    function getEthSignedMessageHash(
-        bytes32 _messageHash
-    ) public pure returns (bytes32) {
-        /*
-        Signature is produced by signing a keccak256 hash with the following format:
-        "\x19Ethereum Signed Message\n" + len(msg) + msg
-        */
-        return
-            keccak256(
-                abi.encodePacked(
-                    "\x19Ethereum Signed Message:\n32",
-                    _messageHash
-                )
-            );
-    }
-
-    /* 4. Verify signature
-    signer = 0xB273216C05A8c0D4F0a4Dd0d7Bae1D2EfFE636dd
-    to = 0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
-    amount = 123
-    message = "coffee and donuts"
-    nonce = 1
-    signature =
-        0x993dab3dd91f5c6dc28e17439be475478f5635c92a56e17e82349d3fb2f166196f466c0b4e0c146f285204f0dcb13e5ae67bc33f4b888ec32dfe0a063e8f3f781b
-    */
-    function verify(
-        OrderStructs.Maker memory order,
-        address _signer,
-        bytes memory signature
-    ) public pure returns (bool) {
-        bytes32 messageHash = getMessageHash(order);
-        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
-
-        return recoverSigner(ethSignedMessageHash, signature) == _signer;
-    }
-
-    function recoverSigner(
-        bytes32 _ethSignedMessageHash,
-        bytes memory _signature
-    ) public pure returns (address) {
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-
-        return ecrecover(_ethSignedMessageHash, v, r, s);
     }
 
     function splitSignature(
         bytes memory sig
-    ) public pure returns (bytes32 r, bytes32 s, uint8 v) {
-        require(sig.length == 65, "invalid signature length");
+    ) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
+        require(sig.length == 65);
 
         assembly {
-            /*
-            First 32 bytes stores the length of the signature
-
-            add(sig, 32) = pointer of sig + 32
-            effectively, skips first 32 bytes of signature
-
-            mload(p) loads next 32 bytes starting at the memory address p into memory
-            */
-
-            // first 32 bytes, after the length prefix
+            // first 32 bytes, after the length prefix.
             r := mload(add(sig, 32))
-            // second 32 bytes
+            // second 32 bytes.
             s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
+            // final byte (first byte of the next 32 bytes).
             v := byte(0, mload(add(sig, 96)))
         }
 
-        // implicitly return (r, s, v)
+        return (v, r, s);
+    }
+
+    function verify(
+        OrderStructs.Maker memory maker,
+        address signer,
+        bytes memory sig
+    ) public view returns (bool) {
+        bytes32 digest = keccak256(
+            abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, hashStruct(maker))
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
+
+        return ecrecover(digest, v, r, s) == signer;
     }
 }
