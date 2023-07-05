faf14b926070673e45a1310551b306573922e5f0
moojing <starts789@gmail.com>
2023-07-06 00:29:57 +0800

    refactor: remove unused file.

--- order-server/src/pages/index.tsx
+++ order-server/src/pages/index.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
-
 import { SignTypedDataVersion, TypedDataUtils, TypedMessage } from "@metamask/eth-sig-util";
-import '../utils/signature';
 
 
 
